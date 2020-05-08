fe120ab882f10e483610ee1a8b2ec6462e0d5fb9
Christopher Woggon <christopher.woggon@gmail.com>
2020-05-08 11:31:56 +0200

    Removing binaries

--- Webots/Roboter_0505/controllers/my_controller/build/release/my_controller.d
+++
@@ -1,9 +0,0 @@
-build/release/my_controller.o: my_controller.cpp \
- C:\Users\Dean_84\AppData\Local\Programs\Webots/include/controller/cpp/webots/LightSensor.hpp \
- C:\Users\Dean_84\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/Dean_84/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:\Users\Dean_84\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- C:\Users\Dean_84\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
- C:\Users\Dean_84\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\Dean_84\AppData\Local\Programs\Webots/include/controller/cpp/webots/camera.hpp \
- C:/Users/Dean_84/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h
