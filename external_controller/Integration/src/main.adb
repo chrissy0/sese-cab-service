3524fdee5cb081e17d4c88d6356ef5606efb581c
Maximilian Weisenseel <weisenseel@campus.tu-berlin.de>
2020-06-02 12:49:38 +0200

    further optimization

--- external_controller/Integration/src/main.adb
+++ external_controller/Integration/src/main.adb
@@ -16,27 +16,29 @@ procedure Main is
    leftSpeed :  Long_Float;
    rightSpeed :  Long_Float;
    leftSpeed_old : Long_Float;
-   righSpeed_old : Long_Float;
+   rightSpeed_old : Long_Float;
 
 begin
   Put_Line("STUFF");
    r := new wc2ec_thread_t; -- allocate and start thread
    leftSpeed_old := 0.0;
-   righSpeed_old := 0.0;
+   rightSpeed_old := 0.0;
 
-   for i in 0 .. 10 loop
+   while WC2EC.ready /= true loop
       delay 1.0;
    end loop;
 
    for I in 0..200000 loop
       rc := Webots_Function.follow_Line(6.0, leftSpeed, rightSpeed);
-      if (leftSpeed_old /= leftSpeed or righSpeed_old /= rightSpeed) then
-        WC2EC.set_motor_sensor_data("wheel1", leftSpeed);
-        WC2EC.set_motor_sensor_data("wheel3", leftSpeed);
+      if (leftSpeed_old /= leftSpeed) then
+         WC2EC.set_motor_sensor_data("wheel1", leftSpeed);
+         WC2EC.set_motor_sensor_data("wheel3", leftSpeed);
+         leftSpeed_old := leftSpeed;
+      end if;
+      if (rightSpeed_old /= rightSpeed) then
         WC2EC.set_motor_sensor_data("wheel2", rightSpeed);
         WC2EC.set_motor_sensor_data("wheel4", rightSpeed);
-        leftSpeed_old := leftSpeed;
-        righSpeed_old := rightSpeed;
+        rightSpeed_old := rightSpeed;
       end if;
 
       delay 0.01;
