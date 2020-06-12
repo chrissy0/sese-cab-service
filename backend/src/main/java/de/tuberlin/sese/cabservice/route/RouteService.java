d3e4e92a5d1e9ad260b31456ccd48e5b8880afe8
Christopher Woggon <christopher.woggon@gmail.com>
2020-06-12 23:30:22 +0200

    Requesting and persisting routes is now possible

---
+++ backend/src/main/java/de/tuberlin/sese/cabservice/route/RouteService.java
@@ -0,0 +1,36 @@
+package de.tuberlin.sese.cabservice.route;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static de.tuberlin.sese.cabservice.route.RouteActionEntity.Action.WAIT;
+import static java.util.Collections.singletonList;
+
+@Service
+@RequiredArgsConstructor
+public class RouteService {
+
+    private final RouteRepo repo;
+
+    public RouteEntity getRoute(Long cabId, Integer version) {
+        // TODO Placeholder code
+
+        Optional<RouteEntity> routeOptional = repo.findById(cabId);
+
+        if (routeOptional.isPresent()) {
+            RouteEntity route = routeOptional.get();
+            route.setVersion(route.getVersion() + 1);
+            return route;
+        } else {
+            return RouteEntity.builder()
+                    .version(0)
+                    .routeActions(singletonList(RouteActionEntity.builder()
+                            .action(WAIT)
+                            .marker(0)
+                            .build()))
+                    .build();
+        }
+    }
+}
