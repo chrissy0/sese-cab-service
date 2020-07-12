34f4d0e2fc10f6a57b97f958cbf4784117b004eb
Christopher Woggon <christopher.woggon@gmail.com>
2020-07-12 12:13:43 +0200

    Updated frontend tab text

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle('Bookr - Autonomous Cab Service');
+  }
 }
